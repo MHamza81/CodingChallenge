0d16702aa52e3719485ed74d5787bf5efbdd77b1
Muhammad Hamza <mhamza0081@gmail.com>
2023-08-12 17:58:35 +0500

    chore: added try/catch to handle errors at controller level

--- src/controllers/GeoJsonController.js
+++ src/controllers/GeoJsonController.js
@@ -7,21 +7,28 @@ import ResponseService from '../services/ResponseService.js';
 export default class GeoJsonController {
   static async getGeoJsonFeatures(req, res) {
     const responseService = new ResponseService(res);
-    const errors = validationResult(req);
+    try {
+      const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      responseService.buildResponse(400, { errors: errors.array() });
+      if (!errors.isEmpty()) {
+        responseService.buildResponse(400, { errors: errors.array() });
+
+        return responseService.sendResponse();
+      }
+
+      const { bbox } = req.query;
+      const osmData = await OsmDataRequestService.fetchOsmData(bbox);
+
+      const jsonData = OsmToJsonService.toJSON(osmData);
+
+      responseService.buildResponse(200, jsonData);
+
+      return responseService.sendResponse();
+    } catch (err) {
+      console.log(err);
+      responseService.buildResponse(502, 'Internal Server Error');
 
       return responseService.sendResponse();
     }
-
-    const { bbox } = req.query;
-    const osmData = await OsmDataRequestService.fetchOsmData(bbox);
-
-    const jsonData = OsmToJsonService.toJSON(osmData);
-
-    responseService.buildResponse(200, jsonData);
-
-    return responseService.sendResponse();
   }
 }
