f252053a25456e5dd1fce602cb8da75b470a0c00
Muhammad Hamza <mhamza0081@gmail.com>
2023-08-13 06:02:59 +0500

    feat: enabled cors in the application
    - updated server to use cors.
    - reading allowed origins from environment variable.
    - updated .env.sample file.

--- src/Server.js
+++ src/Server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+
 import MainRouter from './routes/MainRouter.js';
 
 export default class Server {
@@ -12,6 +14,14 @@ export default class Server {
 
   config() {
     this.app.use(express.json());
+    this.app.use(cors(this.corsOptions));
+  }
+
+  configureCorsOptions() {
+    const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+    this.corsOptions = {
+      origin: allowedOrigins,
+    };
   }
 
   routerConfig() {
